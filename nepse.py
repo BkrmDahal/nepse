04ceaa10e1094e7f634b00492ec16dae15f373f7
Bikram Dahal <bkrm.dahal@gmail.com>
2017-10-29 00:11:38 +0630

    Add files via upload

--- nepse.py
+++ nepse.py
@@ -1,39 +1,39 @@
 """get data from npse"""
 import re
+import time
 from collections import defaultdict
 from functools import lru_cache
 
 import pandas as pd
 
-from util_nepse import get_text, get_soup, 
-					chunks, get_date_range, write_csv, clean_names
+from util_nepse import get_text, get_soup, chunks, get_date_range, write_csv, clean_names
 
 
 #-------------------------------------------------------------------
 def company_listed(url='http://www.nepalstock.com/company?_limit=500', 
                    table='my-table table') -> list:
-	"""get all company listed
-	   
-	   Params:
-	   -------
-	   url: url of page
-	   table: class id of table
-	   
-	   Return
-	   -------
-	   list of information in table as list of list"""
-	   
+    """get all company listed
+        
+        Params:
+        -------
+        url: url of page
+        table: class id of table
+        
+        Return
+        -------
+        list of information in table as list of list"""
+        
     npse = get_soup(url)
     table = npse.find('table', {'class':table})
 
     #get all row 
     rows = table.find_all('tr')[1:-1] #remove  first and last row
-	body_list = []
+    body_list = []
     for row in rows:
         body = row.find_all('td')
         link = row.find('a').get('href') # add company link
         body_list.append(get_text(body)+[link])
-		
+
     colnames  = clean_names(body_list[0])
     colnames = colnames[:-1] + ['link']
     body_list[0] =  colnames  # first list is colnames
@@ -44,28 +44,28 @@ company = company_listed()
 # I like converting to DataFrame for analysis and wangling
 company = pd.DataFrame(company[1:], columns=company[0])
 company['url_id'] = company['link'].str.split('/').str[-1]
+print(company.shape)
 
 #------------------------------------------------------------------------
-def company_detail(company_id = '397', 
-				   table='my-table table'):
+def company_detail(company_id = '397', table='my-table table'):
     """Get company additional detail
-	   Params:
-	   -------
-	   company_id: company display id
-	   table: class id of table
-	   
-	   Return
-	   -------
-	   list of information in table as list of list"""
+    Params:
+    -------
+    company_id: company display id
+    table: class id of table
+
+    Return
+    -------
+    list of information in table as list of list"""
     url = 'http://www.nepalstock.com/company/display/{}'.format(company_id)
     npse = get_soup(url)
     table = npse.find('table', {'class':table})
     rows = table.find_all('tr')[2:] #remove  unwanted row
-	body_list = []
+    body_list = []
     for i in rows:
         body = i.find_all('td')
         body_list.append(get_text(body))
-		
+
     #remove unwanted row
     del body_list[0][0], body_list[1]
     
@@ -73,36 +73,51 @@ def company_detail(company_id = '397',
     return body_list
 
 #get detail of all company
-detail = defaultdict(list)
-for i in company['url_id']:
-    c_detail = company_detail(i)
-    for key, values in c_detail:
-        detail[key].append(values)
-detail = pd.DataFrame(detail)
+def company_detail_all(company_id_list, table):
+    """Get company stail
+    Params:
+    -------
+    company_id: list of company display id
+    table: class id of table
 
+    Return
+    -------
+    list of information in table as dict"""
+
+    detail = defaultdict(list)
+    for i in company_id_list:
+        c_detail = company_detail(i)
+        for key, values in c_detail:
+            detail[key].append(values)
+    return detail
+
+detail = company_detail_all(company['url_id'][1:3], table='my-table table')
+detail = pd.DataFrame(detail)
+print(detail)
 ##join company with detail
 company = pd.merge(company, detail, how='left', on=['url_id'])
 company.to_csv('company_full.csv')
 
 #--------------------------------------------------------------------------
+@lru_cache(maxsize=512)
 def stock_today(date, 
-				table='table table-condensed table-hover', 
-				headers = True):
+                table='table table-condensed table-hover', 
+                headers = True):
     """Get data from stock table
-	   Params:
-	   -------
-	   date: 'str' -- 'YYYY-MM-DD'
-	   table: class id of table
-	   headers: if you want to return header of table
-	   
-	   Return
-	   -------
-	   list of information in table as list of list"""
-	   
+        Params:
+        -------
+        date: 'str' -- 'YYYY-MM-DD'
+        table: class id of table
+        headers: if you want to return header of table
+        
+        Return
+        -------
+        list of information in table as list of list"""
+        
     url = ('http://www.nepalstock.com/todaysprice?'
            +'startDate={}&stock-symbol=&_limit=500'.format(date))
     npse = get_soup(url)
-	pages = npse.find_all(text=re.compile('Page.*')) #'Page' is only present if page has data
+    pages = npse.find_all(text=re.compile('Page.*')) #'Page' is only present if page has data
     if pages:
         table = npse.find('table', {'class':table})
 
@@ -125,21 +140,23 @@ def stock_today(date,
         print("No data on stock exchange, was that day public holiday!!")
         pass
 
+
 def stockprice_range(start, end):
-	"""get stock price of each company each day for given range
-	
-		Params
-		--------
-		start: str -- 'YYYY-MM-DD'
-		end: str -- 'YYYY-MM-DD'
-		
-		Return
-		-------
-		list of information as list of list, first list as header"""
-		
+    """get stock price of each company each day for given range
+
+        Params
+        --------
+        start: str -- 'YYYY-MM-DD'
+        end: str -- 'YYYY-MM-DD'
+        
+        Return
+        -------
+        list of information as list of list, first list as header"""
+        
     ranges = get_date_range(start, end)
     stock = []
     for date, day in ranges:
+        time.sleep(1)
         if day not in [5, 4]: #stock market is closed on fri and sat
             if not stock:
                 temp = stock_today(date)
@@ -150,14 +167,13 @@ def stockprice_range(start, end):
                 if temp:
                     stock += temp
     return stock
-	
-stock_all = stockprice_range('2017-01-01', '2017-10-25')
-stock_all = pd.DataFrame(stock_all[1:], columns=stock_all[0])
-stock_all.to_csv('stock_range.csv')
 
-#-----------------------------------------------------------------------------------#
+years = [ '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01', '2016-01-01', '2017-01-01', '2017-10-27']
+for i, year in enumerate(years):
+    if i+1 != 7:
+        stock_all = stockprice_range(years[i], years[i+1])
+        stock_all = pd.DataFrame(stock_all[1:], columns=stock_all[0])
+        stock_all.to_csv('stock_range.csv',mode='a', header=False)
 
-
-
-
-
+#------------------------------------------------------------------------------#
+x = 1
